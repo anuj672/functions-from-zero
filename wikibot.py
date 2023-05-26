4d13229ad5263e0bc4e6e3d7321b6fd8db95df26
Anuj Naresh Chetwani <71286830+anuj672@users.noreply.github.com>
2023-05-26 21:30:28 +0000

    click command on bot

--- wikibot.py
+++ wikibot.py
@@ -1,7 +1,12 @@
 import wikipedia
+import click
 
+@click.command()
+@click.option('--name', prompt='Wikipedia page to scrape',
+              help='web page to scrape')
 def scrape(name="microsoft",length=1):
     result= wikipedia.summary(name, sentences=length)
-    return result
+    click.echo(click.style(f"{result}", bg="blue",fg="red"))
 
-print(scrape("facebook"))
+if __name__ == '__main__':
+    scrape()

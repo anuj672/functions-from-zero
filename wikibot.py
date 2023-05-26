229fbe5c68e7da1706d1e6cb28eefb28958d73f2
Anuj Naresh Chetwani <71286830+anuj672@users.noreply.github.com>
2023-05-26 22:21:24 +0000

    clean call

--- wikibot.py
+++ wikibot.py
@@ -5,7 +5,7 @@ from mylib.bot import scrape
 @click.option('--name', prompt='Wikipedia page to scrape',
               help='web page to scrape')
 def cli(name="microsoft",length=1):
-    result= scrape(name, sentences=length)
+    result= scrape(name,length)
     click.echo(click.style(f"{result}", bg="blue",fg="red"))
 
 if __name__ == '__main__':

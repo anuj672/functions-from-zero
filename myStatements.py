4ab6d5a74b73aa7dee7229026eea28e2ef4c3cd3
Anuj Naresh Chetwani <71286830+anuj672@users.noreply.github.com>
2023-05-26 13:13:47 +0000

    add scaffold

--- myStatements.py
+++ myStatements.py
@@ -5,7 +5,7 @@ for item in my_list:
     print(f"my fav thing to eat is : {item}")
 
 for _,v in my_dict.items():
-      print(f"my fav thing to drink is : {v}") 
+    print(f"my fav thing to drink is : {v}") 
 
 ##need to check these automatically (build)
 #my_dict = my_dict #syntax bug

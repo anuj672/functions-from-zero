49b1debb96b2f80c7854d87895d883fecdd41dab
Anuj Naresh Chetwani <71286830+anuj672@users.noreply.github.com>
2023-05-27 11:10:28 +0000

    added indent

--- test_wikibot.py
+++ test_wikibot.py
@@ -3,11 +3,12 @@ from wikibot import cli
 from click.testing import CliRunner
 
 def test_scrape():
+
     assert "Microsoft" in scrape('microsoft')
 
-
-def test_wikibot(): #name of the file
-  runner = CliRunner()
-  result = runner.invoke(cli, ['--name','Microsoft', '--length','1'])
-  assert result.exit_code == 0
-  assert 'Microsoft' in result.output    
+def test_wikibot():
+     #name of the file
+    runner = CliRunner()
+    result = runner.invoke(cli, ['--name','Microsoft', '--length','1'])
+    assert result.exit_code == 0
+    assert 'Microsoft' in result.output    

4d47dd8e7bdfbc7e6f8b8bbc7a54c588594e14c7
Anuj Naresh Chetwani <71286830+anuj672@users.noreply.github.com>
2023-05-27 11:07:02 +0000

    added tests for cli

--- test_wikibot.py
+++ test_wikibot.py
@@ -1,4 +1,13 @@
 from mylib.bot import scrape
+from wikibot import cli
+from click.testing import CliRunner
 
 def test_scrape():
     assert "Microsoft" in scrape('microsoft')
+
+
+def test_wikibot(): #name of the file
+  runner = CliRunner()
+  result = runner.invoke(cli, ['--name','Microsoft', '--length','1'])
+  assert result.exit_code == 0
+  assert 'Microsoft' in result.output    
